852eaac6d952fdad098065a4175a5708d0f8b657
karanbgit <karanbarale7047@gmail.com>
2024-11-15 14:04:33 +0530

    create Intigration controller

---
+++ application/controllers/Intigration.php
@@ -0,0 +1,12 @@
+<?php
+
+defined("BASEPATH") or ("No Direct Script Access Allowed");
+class Intigration extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+}
+?>
